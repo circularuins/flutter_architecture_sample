e41c4ecdc433f8691ac5b9699783cbe0bc26eba4
circularuins <natsuhiko.wake@gmail.com>
2020-01-29 14:55:53 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:architecture_test/top_page_1.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        title: 'Flutter Demo Home page',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: const Text("setStateの場合"),
+            onTap: () {
+              Navigator.push<void>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TopPage1(),
+                    fullscreenDialog: true,
+                  ));
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/top_page_3_0.dart
@@ -0,0 +1,89 @@
+import 'package:architecture_test/count_repository.dart';
+import 'package:architecture_test/counter_bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TopPage3_0 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage3_0> {
+  CounterBloc counterBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    counterBloc = CounterBloc(CountRepository());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bloc Simple Demo'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _WidgetA(counterBloc),
+          _WidgetB(),
+          _WidgetC(counterBloc),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    counterBloc.dispose();
+    super.dispose();
+  }
+}
+
+class _WidgetA extends StatelessWidget {
+  final CounterBloc counterBloc;
+
+  _WidgetA(this.counterBloc);
+
+  @override
+  Widget build(BuildContext context) {
+    print('called _WidgetA#build()');
+    return Center(
+      child: StreamBuilder(
+        stream: counterBloc.value,
+        builder: (context, snapshot) {
+          return Text(
+            '${snapshot.data}',
+            style: Theme.of(context).textTheme.display1,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _WidgetB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print('called _WidgetB#build()');
+    return const Text('I am a widget that will not be rebuilt.');
+  }
+}
+
+class _WidgetC extends StatelessWidget {
+  final CounterBloc counterBloc;
+
+  _WidgetC(this.counterBloc);
+
+  @override
+  Widget build(BuildContext context) {
+    print('called _WidgetC#build()');
+    return RaisedButton(
+      onPressed: () {
+        counterBloc.incrementCounter();
+      },
+      child: Icon(Icons.add),
+    );
+  }
+}
